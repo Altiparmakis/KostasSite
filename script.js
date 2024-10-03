e874cd082e26cd1f38c4eacf0ad0e55a9ee98c20
Altiparmakis <kwnstantinosalti@gmail.com>
2024-10-03 22:51:45 +0300

    add another project

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const toggleSwitch = document.getElementById("darkModeSwitch");
 const mode = document.querySelector(".mode");
 const firstSection = document.querySelector(".sc-section");
 const body = document.body;
-const projects = document.querySelector(".cont22");
+const projects = document.querySelectorAll(".cont22");
 
 toggleSwitch.addEventListener("change", () => {
   if (toggleSwitch.checked) {
@@ -10,12 +10,17 @@ toggleSwitch.addEventListener("change", () => {
     firstSection.classList.remove("bg-body-tertiary");
     firstSection.classList.add("new");
     body.classList.add("new");
-    projects.classList.add("new2");
+    projects.forEach((project) => {
+      project.classList.add("new2");
+    });
   } else {
     mode.innerHTML = "light";
     firstSection.classList.add("bg-body-tertiary");
     firstSection.classList.remove("new");
     body.classList.add();
     body.classList.remove("new");
+    projects.forEach((project) => {
+      project.classList.remove("new2");
+    });
   }
 });
