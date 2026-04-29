872343618fec02f0f9ac8d4bcb0de9cc6032bc15
Altiparmakis <kwnstantinosalti@gmail.com>
2026-04-29 22:33:14 +0300

    changes

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
 const toggleSwitch = document.getElementById("darkModeSwitch");
 const mode = document.querySelector(".mode");
-const firstSection = document.querySelector(".sc-section");
 const body = document.body;
-const projects = document.querySelectorAll(".cont22");
 
 toggleSwitch.addEventListener("change", () => {
-  if (toggleSwitch.checked) {
-    mode.innerHTML = "dark";
-    firstSection.classList.remove("bg-body-tertiary");
-    firstSection.classList.add("new");
-    body.classList.add("new");
-    projects.forEach((project) => {
-      project.classList.add("new2");
-    });
-  } else {
-    mode.innerHTML = "light";
-    firstSection.classList.add("bg-body-tertiary");
-    firstSection.classList.remove("new");
-    body.classList.add();
-    body.classList.remove("new");
-    projects.forEach((project) => {
-      project.classList.remove("new2");
-    });
-  }
+  const isDark = toggleSwitch.checked;
+  body.classList.toggle("dark-mode", isDark);
+  mode.textContent = isDark ? "dark" : "light";
 });
